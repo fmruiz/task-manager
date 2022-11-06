bd2a4d52e33815b67eb6f05256446fbbfcb270a6
fmruiz <x.francoruiz@gmail.com>
2022-11-06 18:01:06 -0300

    data validation and sanitization of User model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
@@ -9,9 +10,28 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true,
     },
     age: {
         type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        },
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        },
     },
 });
 
