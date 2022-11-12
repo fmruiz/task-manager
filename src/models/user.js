9cc259c46c779912a4b7bf9c72c1a242e98a4f42
fmruiz <x.francoruiz@gmail.com>
2022-11-12 16:20:28 -0300

    add timestamp

--- src/models/user.js
+++ src/models/user.js
@@ -6,56 +6,65 @@ const jwt = require('jsonwebtoken');
 /**
  * User Schrema
  */
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
         },
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if (value.length < 6) {
-                throw new Error('Password must be greater than 6 characters');
-            }
-
-            if (value.includes('password')) {
-                throw new Error('Password should not contains password word');
-            }
-        },
-    },
-    tokens: [
-        {
-            token: {
-                type: String,
-                required: true,
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number');
+                }
             },
         },
-    ],
-});
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid');
+                }
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (value.length < 6) {
+                    throw new Error(
+                        'Password must be greater than 6 characters'
+                    );
+                }
+
+                if (value.includes('password')) {
+                    throw new Error(
+                        'Password should not contains password word'
+                    );
+                }
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 /**
  * Create an static method
